319843e1eb6dd802da110e08f9dd42c404c05175
knyghtspup <knyghtspup@10.30.16.44>
2017-02-17 13:55:30 -0600

    First Commit From Laptop

---
+++ src/lab1/IntroToProgrammingCourse.java
@@ -0,0 +1,74 @@
+package lab1;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Describe responsibilities here. NOTE: this class has NO PREREQUISITES!
+ * Do not change this fact.
+ *
+ * @author      your name goes here
+ * @version     1.00
+ */
+public class IntroToProgrammingCourse extends TechClass{
+    
+    @Override
+    public String getCourseName() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public double getCredits() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String getCourseNumber() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public String getPrerequisites() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public void setCourseName(String CourseName) {
+        if(courseName == null || courseName.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseName cannot be null of empty string");
+            System.exit(0);
+        }
+        this.courseName = courseName;
+    }
+    
+    @Override
+    public void setCourseNumber(String courseNumber) {
+        if(courseNumber == null || courseNumber.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: courseNumber cannot be null of empty string");
+            System.exit(0);
+        }
+        this.setCourseNumber() = courseNumber;
+    }
+    
+    @Override
+    public void setCredits(double credits) {
+        if(credits < 0.5 || credits > 4.0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.credits = credits;
+    }
+    
+    @Override
+    public void setPrerequisites(String prerequisites) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public IntroToProgrammingCourse(String courseName, String courseNumber) {
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
+    }
+
+}
